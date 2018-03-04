4a6b2fbf5f1bc1b0d91585eb91333476394653a6
Laetitia Laversa <laetitialaversa2904@hotmail.fr>
2018-03-04 17:51:07 +0100

    Début code propre
    Création d'une classe fichier qui s'occupe de la prise en charge du fichier -> parser + création des objets pour cette instance.

--- src/main/java/hashCode2018/App.java
+++ src/main/java/hashCode2018/App.java
@@ -12,72 +12,75 @@ public class App
 {
     public static void main( String[] args ){
 
-        ArrayList<Trajet> tabloTrajet = new ArrayList<Trajet>();
-        ArrayList<Voiture> tabloVoiture = new ArrayList<Voiture>();
-        String filepath = "/home/ringo/Bureau/hashCode2018/e_high_bonus.in";
+//        ArrayList<Trajet> tabloTrajet = new ArrayList<Trajet>();
+//        ArrayList<Voiture> tabloVoiture = new ArrayList<Voiture>();
+//        String filepath = "/Users/Laetitia/Documents/hashCode2018/a_example.in";
         int points = 0;
+//
+//
+//        try {
+//            RandomAccessFile file = new RandomAccessFile(filepath, "r");
+//            String str;
+//            int i = 0;
+//            while ((str = file.readLine()) != null) { //par ligne
+//
+//                ArrayList<Integer> firstline = new ArrayList();
+//                Scanner s = new Scanner(str);
+//                while (s.hasNextInt()) {
+//                    firstline.add(s.nextInt());
+//                }
+//
+//                //System.out.println(str);
+//                if( i == 0){ //infos de base
+//                    int nbVoiture = firstline.get(2);
+//                    int nbTrajet = (Integer)firstline.get(3);
+//                    int bonus = (Integer)firstline.get(4);
+//                    int maxsteps = (Integer)firstline.get(5);
+//
+//                    for(int j = 0; j < nbVoiture;j++){
+//                        Voiture v = new Voiture();
+//                        v.setDispoMax(maxsteps);
+//                        tabloVoiture.add(v);
+//                    }
+//
+//                    for(int j = 0; j < nbTrajet; j++){
+//                        Trajet t = new Trajet();
+//                        t.setBonus(bonus);
+//                        tabloTrajet.add(t);
+//                    }
+//
+//                }else{ //rides
+//                    Trajet t = tabloTrajet.get(i-1);
+//                    t.setStartX((Integer)firstline.get(0));
+//                    t.setStartY((Integer)firstline.get(1));
+//                    t.setEndX((Integer)firstline.get(2));
+//                    t.setEndY((Integer)firstline.get(3));
+//                    t.setEarlierStart((Integer)firstline.get(4));
+//                    t.setLatestEnd((Integer)firstline.get(5));
+//                    t.setDistance(Math.abs(t.endX - t.startX) + Math.abs(t.endY - t.startY));
+//
+//                }
+//            i++;
+//            }//fin while parser
+//            file.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    	
+    	Fichier f1 = new Fichier("/Users/Laetitia/Documents/hashCode2018/a_example.in"); 
+    	
 
-
-        try {
-            RandomAccessFile file = new RandomAccessFile(filepath, "r");
-            String str;
-            int i = 0;
-            while ((str = file.readLine()) != null) { //par ligne
-
-                ArrayList<Integer> firstline = new ArrayList();
-                Scanner s = new Scanner(str);
-                while (s.hasNextInt()) {
-                    firstline.add(s.nextInt());
-                }
-
-                //System.out.println(str);
-                if( i == 0){ //infos de base
-                    int nbVoiture = firstline.get(2);
-                    int nbTrajet = (Integer)firstline.get(3);
-                    int bonus = (Integer)firstline.get(4);
-                    int maxsteps = (Integer)firstline.get(5);
-
-                    for(int j = 0; j < nbVoiture;j++){
-                        Voiture v = new Voiture();
-                        v.setDispoMax(maxsteps);
-                        tabloVoiture.add(v);
-                    }
-
-                    for(int j = 0; j < nbTrajet; j++){
-                        Trajet t = new Trajet();
-                        t.setBonus(bonus);
-                        tabloTrajet.add(t);
-                    }
-
-                }else{ //rides
-                    Trajet t = tabloTrajet.get(i-1);
-                    t.setStartX((Integer)firstline.get(0));
-                    t.setStartY((Integer)firstline.get(1));
-                    t.setEndX((Integer)firstline.get(2));
-                    t.setEndY((Integer)firstline.get(3));
-                    t.setEarlierStart((Integer)firstline.get(4));
-                    t.setLatestEnd((Integer)firstline.get(5));
-                    t.setDistance(Math.abs(t.endX - t.startX) + Math.abs(t.endY - t.startY));
-
-                }
-            i++;
-            }//fin while parser
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Collections.sort(tabloTrajet);
+        Collections.sort(f1.tabloTrajet);
 
         ArrayList<ArrayList<Integer>> resultat = new ArrayList<ArrayList<Integer>>();
-        for(Voiture v : tabloVoiture){
+        for(Voiture v : f1.tabloVoiture){
             resultat.add(new ArrayList<Integer>());
         }
         //Jerem version
-        for(int i = 0 ; i < tabloVoiture.size(); i++){
-            for(int j = 0; j < tabloTrajet.size(); j++){
-                Trajet t = tabloTrajet.get(j);
-                Voiture v = tabloVoiture.get(i);
+        for(int i = 0 ; i < f1.tabloVoiture.size(); i++){
+            for(int j = 0; j < f1.tabloTrajet.size(); j++){
+                Trajet t = f1.tabloTrajet.get(j);
+                Voiture v = f1.tabloVoiture.get(i);
                 if(!(t.done)){
                     if(v.dispo == t.earlierStart && v.dispo + t.distance + v.distance(t) < v.dispoMax){
                         //if(v.x == t.startX && v.y == t.startY && v.dispo >= t.earlierStart && t.latestEnd > Math.max(v.dispo,t.earlierStart) + v.distance(t) + t.distance){
@@ -98,10 +101,10 @@ public class App
             }
         }
 
-        for(int i = 0 ; i < tabloVoiture.size(); i++) {
-            for (int j = 0; j < tabloTrajet.size(); j++) {
-                Trajet t = tabloTrajet.get(j);
-                Voiture v = tabloVoiture.get(i);
+        for(int i = 0 ; i < f1.tabloVoiture.size(); i++) {
+            for (int j = 0; j < f1.tabloTrajet.size(); j++) {
+                Trajet t = f1.tabloTrajet.get(j);
+                Voiture v = f1.tabloVoiture.get(i);
                 if (!(t.done)) {
                     if(v.dispo + t.distance + v.distance(t) < v.dispoMax &&
                             t.latestEnd > Math.max(v.dispo,t.earlierStart) + v.distance(t) + t.distance){
@@ -173,12 +176,12 @@ public class App
 //        }
 
         int cpt = 0;
-        for(Trajet t : tabloTrajet){
+        for(Trajet t : f1.tabloTrajet){
             if(!t.done){
                 cpt++;
             }
         }
-        System.out.println("Total trajet -> " + tabloTrajet.size());
+        System.out.println("Total trajet -> " + f1.tabloTrajet.size());
         System.out.println("Pas pris -> " + cpt);
         //fin for jerem version
 
---
+++ src/main/java/hashCode2018/Fichier.java
@@ -0,0 +1,75 @@
+package hashCode2018;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Fichier {
+	
+	int nbVoiture; 
+	int nbTrajet; 
+	int bonus; 
+	int maxsteps;
+
+    ArrayList<Trajet> tabloTrajet = new ArrayList<Trajet>();
+    ArrayList<Voiture> tabloVoiture = new ArrayList<Voiture>();
+	
+	String filepath;
+	
+	Fichier(String fp){ 
+		this.filepath = fp; 
+		try {
+            RandomAccessFile file = new RandomAccessFile(this.filepath, "r");
+            String str;
+            int i = 0;
+            while ((str = file.readLine()) != null) { //par ligne
+
+                ArrayList<Integer> firstline = new ArrayList();
+                Scanner s = new Scanner(str);
+                while (s.hasNextInt()) {
+                    firstline.add(s.nextInt());
+                }
+
+                //System.out.println(str);
+                if( i == 0){ //infos de base
+                    this.nbVoiture = firstline.get(2);
+                    this.nbTrajet = (Integer)firstline.get(3);
+                    this.bonus = (Integer)firstline.get(4);
+                    this.maxsteps = (Integer)firstline.get(5);
+
+                    for(int j = 0; j < this.nbVoiture;j++){
+                        Voiture v = new Voiture();
+                        v.setDispoMax(this.maxsteps);
+                        this.tabloVoiture.add(v);
+                    }
+
+                    for(int j = 0; j < this.nbTrajet; j++){
+                        Trajet t = new Trajet();
+                        t.setBonus(this.bonus);
+                        this.tabloTrajet.add(t);
+                    }
+
+                }else{ //rides
+                    Trajet t = this.tabloTrajet.get(i-1);
+                    t.setStartX((Integer)firstline.get(0));
+                    t.setStartY((Integer)firstline.get(1));
+                    t.setEndX((Integer)firstline.get(2));
+                    t.setEndY((Integer)firstline.get(3));
+                    t.setEarlierStart((Integer)firstline.get(4));
+                    t.setLatestEnd((Integer)firstline.get(5));
+                    t.setDistance(Math.abs(t.endX - t.startX) + Math.abs(t.endY - t.startY));
+
+                }
+            i++;
+            }//fin while parser
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+		
+	}
+	
+	
+}
