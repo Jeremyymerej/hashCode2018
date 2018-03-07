7d2f03072058aaf099344f117e2020f846d8b5d3
Laetitia Laversa <laetitialaversa2904@hotmail.fr>
2018-03-07 15:21:24 +0100

    Résultats greedy2 + nouveau comptage de points
    Possibilité de recompter les points d'une seule voiture : BEAUCOUP plus rapide

--- src/main/java/hashCode2018/Acteurs/Points.java
+++ src/main/java/hashCode2018/Acteurs/Points.java
@@ -8,72 +8,86 @@ public class Points {
 	public int points = 0;
 	public ArrayList<Trajet> trajets = new ArrayList<Trajet>(); //version non triée!
 	public ArrayList<Voiture> voitures = new ArrayList<Voiture>();
+	
+	public ArrayList<Integer> ptsVoiture = new ArrayList<Integer>();
 	   
 	    
 	public Points(ArrayList<ArrayList<Integer>> res, ArrayList<Trajet> trajets, ArrayList<Voiture> voitures){
 		this.res = res; 
 		this.voitures = voitures; 
 		this.trajets = trajets;
+		for(int i = 0; i < res.size(); i++){ 
+			ptsVoiture.add(0);
+		}
 	}
 	
 	
 	public void compter(){
-		Voiture v = null;  
-		Trajet t = null; 
-		boolean bonus = false; 
-		int tick = 0; 
-		int b =0; 
-		
-		
 		for(int i = 0 ; i < res.size(); i++ ){
-			tick = 0; 
-			v = voitures.get(i); 
-			v.x = 0; 
-			v.y = 0;
-			for(int j = 0; j < res.get(i).size(); j++ ){
-				t = trajets.get(res.get(i).get(j));
-
-				bonus = false; 
-				// la voiture va la ou la course commence
-				tick += v.distance(t);  
-				v.x = t.startX; 
-				v.y = t.startY; 
-				
-				
-				//on verifie qu'il est pas trop tot pour commencer la course et sinon on attend de pouvoir commencer la course 
-				if ((t.earlierStart - tick) > 0 ){
-					tick += (t.earlierStart -tick); 
-				}
-					
-				// on va donner un bonus si la course commence au bon moment
-				if(t.getEarlierStart() == tick ){ 
-					bonus = true;  
-				}
-					
-				// on regarde quand la course se termine 
-				tick += t.distance; 
-				v.x = t.endX; 
-				v.y = t.endY; 
-					
-				//on donne les points si on respecte les conditions de la course et de la voiture 
-				if(tick <= t.latestEnd && tick <= v.dispoMax){ 
-					points += t.distance; 
-					if(bonus){ 
-						points += t.bonus;
-						b++;
-					}
-				}
-						
-			}
-			
+			compterVoiture(i);			
+		}
+		
+		for(int i = 0; i< voitures.size(); i++){ 
+			points += ptsVoiture.get(i); 
 		}
 		//System.out.println(b);
-		
 	}
 	
 	public void recompter(){ 
 		points = 0; 
 		compter();
+	}
+	
+	public void recompter(int i){ 
+		points = 0; 
+		ptsVoiture.set(i,0); 
+		compterVoiture(i); 
+		for(int j = 0; j< ptsVoiture.size(); j++){ 
+			points += ptsVoiture.get(j); 
+		}
+	}
+	
+	public void compterVoiture(int i){ 
+		int tick = 0; 
+		Voiture v = voitures.get(i); 
+		v.x = 0; 
+		v.y = 0;
+		boolean bonus = false; 
+		Trajet t = null; 
+		for(int j = 0; j < res.get(i).size(); j++ ){
+			t = trajets.get(res.get(i).get(j));
+
+			bonus = false; 
+			// la voiture va la ou la course commence
+			tick += v.distance(t);  
+			v.x = t.startX; 
+			v.y = t.startY; 
+			
+			
+			//on verifie qu'il est pas trop tot pour commencer la course et sinon on attend de pouvoir commencer la course 
+			if ((t.earlierStart - tick) > 0 ){
+				tick += (t.earlierStart -tick); 
+			}
+				
+			// on va donner un bonus si la course commence au bon moment
+			if(t.getEarlierStart() == tick ){ 
+				bonus = true;  
+			}
+				
+			// on regarde quand la course se termine 
+			tick += t.distance; 
+			v.x = t.endX; 
+			v.y = t.endY; 
+				
+			//on donne les points si on respecte les conditions de la course et de la voiture 
+			if(tick <= t.latestEnd && tick <= v.dispoMax){ 
+				ptsVoiture.set(i, ptsVoiture.get(i)+ t.distance);  //ICI
+				if(bonus){ 
+					ptsVoiture.set(i, ptsVoiture.get(i)+ t.bonus); //ICI 
+				}
+			}
+					
+		}
 	}
 
 }
--- src/main/java/hashCode2018/Algo/Greedy2.java
+++ src/main/java/hashCode2018/Algo/Greedy2.java
@@ -8,13 +8,33 @@ import hashCode2018.Acteurs.Voiture;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
+
+/*
+ * Résultat : 
+ * 
+ * A : 10 
+ * B : 96.150
+ * C : 8.188.764
+ * D : 1.437.473
+ * E : 3.016.035
+ * 
+ * 
+ * Résultat en attribuant un trajet a chaque voiture avant : 
+ * 
+ * A : 10
+ * B : 144.124
+ * C : 8.191.377
+ * D : 1.938.075
+ * E : 7.162.098 (calcul pas bon, google dit 13.768.329 )
+ */
 public class Greedy2 extends AbstractAlgorithme {
-	   ArrayList<Trajet> trajets = new ArrayList<Trajet>();
+	   public ArrayList<Trajet> trajets = new ArrayList<Trajet>();
 	    ArrayList<Voiture> voitures = new ArrayList<Voiture>();
-	    ArrayList<ArrayList<Integer>> resultat = new ArrayList<ArrayList<Integer>>();
+	    public ArrayList<ArrayList<Integer>> resultat = new ArrayList<ArrayList<Integer>>();
 
 	    
-	    Greedy2(ArrayList<Trajet> T, ArrayList<Voiture> V){
+	    public Greedy2(ArrayList<Trajet> T, ArrayList<Voiture> V){
 	    	this.trajets = T; 
 	    	this.voitures = V; 
 	    } 
@@ -33,21 +53,28 @@ public class Greedy2 extends AbstractAlgorithme {
 	    	Points score_tmp; 
 	    	int score_max;
 	    	
-	    	for(int i = 0; i < trajets.size(); i++){ 
+	    	//On attribue d'abord un trajet a chaque voiture 
+	    	for(int i = 0; i < voitures.size(); i++){ 
+	    		resultat.get(i).add(i);	
+	    	}
+	    	
+	    	//puis selon les points 
+	    	for(int i =/*0*/ voitures.size(); i < trajets.size(); i++){ 
 	    		//System.out.println("Trajet "+i);
 	    		voiture_max = -1;
 	    		score_tmp = new Points(resultat, trajetsPasTriés, voitures);
 		    	score_max = 0;
 	    		score_tmp.recompter(); 
 	    	 	for(int j = 0; j < voitures.size(); j++){ 
+	    	 		//System.out.println("\tVoiture "+j);
 	    	 		resultat.get(j).add(i);	
-		        	score_tmp.recompter();
+		        	score_tmp.recompter(j);
 		        	if (score_tmp.points > score_max){
 			    		score_max = score_tmp.points; 
 			    		voiture_max = j; 
 			    	}
 			    	resultat.get(j).remove(resultat.get(j).size() -1); 
-			    	score_tmp.recompter();
+			    	score_tmp.recompter(j);
 		    	}
 	    	 	if(voiture_max != -1){ 
 	    	 		//System.out.println("\t Voiture "+ voiture_max);
